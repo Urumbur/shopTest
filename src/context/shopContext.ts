f94203ea391d12d8212bb753160e285965730e43
Urumbur <loladrian299@gmail.com>
2020-12-16 11:46:10 +0100

    Update shopContext.ts

--- src/context/shopContext.ts
+++ src/context/shopContext.ts
@@ -1,9 +1,12 @@
-import { createContext } from 'react';
+import { createContext, useReducer } from 'react';
 
 export const ShopContext = createContext(null);
 
 
 export const ShopContextProvider = ({children}) => {
+  
+  const [store, dispatch] = useReducer(cartReducer, {cart: []})
+  
   return (
     <ShopContext.Provider> {children} </ShopContext.Provider>
   )
