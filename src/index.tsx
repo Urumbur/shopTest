a1609a481caf50a9863809543dc1e430a7c2cf03
Urumbur <loladrian299@gmail.com>
2020-12-16 11:43:51 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ShopContextProvider } from './context/shopContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>  
   </React.StrictMode>,
   document.getElementById('root')
 );
