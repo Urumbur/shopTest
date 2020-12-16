ed042ea4e77c545e4bf9d7eac48ceefe6830b63f
Urumbur <loladrian299@gmail.com>
2020-12-16 12:03:40 +0100

    Update Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import { Link } from 'react-router-dom';
 import { Button } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
+import { ShopContext } from '../../context/shopContext';
 
 const Navbar = () => {
-
+    const {store} = useContext(ShopContext);
     return(
         <div className="navbar">
             <Link className="button" to="/"><Button variant="outlined">Products</Button></Link>
-            <Link className="button" to="/cart"><Button variant="outlined"><ShoppingCart /> ()</Button></Link>
+            <Link className="button" to="/cart"><Button variant="outlined"><ShoppingCart /> ({store.cart.length})</Button></Link>
         </div>
     )
 }
