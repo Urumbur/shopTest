55f8dcb10afa8809ca0457d4ac2ac8b4e1400ea6
Urumbur <loladrian299@gmail.com>
2020-12-16 11:53:30 +0100

    Update action.ts

--- src/reducer/action.ts
+++ src/reducer/action.ts
@@ -1,1 +1,11 @@
+import {ADD_CART, REMOVE_CART} from './type';
 
+export const addCart = (cart) => ({
+  action: ADD_CART,
+  payload: cart
+});
+
+export const removeCart = (cartId) => ({
+  action: REMOVE_CART,
+  payload: cartId
+})
