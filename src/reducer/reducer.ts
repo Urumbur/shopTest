dcf8a4ad3f31c860e4bc150eac758fd407c5a456
Urumbur <loladrian299@gmail.com>
2020-12-16 11:56:30 +0100

    Update reducer.ts

--- src/reducer/reducer.ts
+++ src/reducer/reducer.ts
@@ -1,1 +1,12 @@
+import {ADD_CART, REMOVE_CART} from './type';
 
+export const shopCartReducer = (state, action) => {
+  switch(action.type) {
+    case ADD_CART:
+      return ...state;
+    case REMOVE_CART:
+      return ...state;
+    default:
+      return state;
+  }
+}
